83ef0f1a0408a5f347fe10dbcf06e1a15f15f925
jpg22 <jpg22@tamu.edu>
2023-02-04 17:15:09 -0600

    y

--- src/App.js
+++ src/App.js
@@ -1,41 +1,64 @@
-const people = [{
-	name: 'John Doe',
-	age: 54,
-	hairColor: 'brown',
-	hobbies: ['swimming', 'bicycling', 'video games'],
-}, {
-	name: 'Brenda Smith',
-	age: 33,
-	hairColor: 'black',
-	hobbies: ['golf', 'mathematics'],
-}, {
-	name: 'Jane Garcia',
-	age: 27,
-	hairColor: 'blonde',
-	hobbies: ['biology', 'medicine', 'gymnastics'],
-}];
+import { SmallPersonListItem } from "./people/SmallPerson";
+import { RegularList } from "./RegularList";
+import { LargePersonListItem } from "./people/LargePerson";
 
-const products = [{
-	name: 'Flat-Screen TV',
-	price: '$300',
-	description: 'Huge LCD screen, a great deal',
-	rating: 4.5,
-}, {
-	name: 'Basketball',
-	price: '$10',
-	description: 'Just like the pros use',
-	rating: 3.8,
-}, {
-	name: 'Running Shoes',
-	price: '$120',
-	description: 'State-of-the-art technology for optimum running',
-	rating: 4.2,
-}];
+const people = [
+  {
+    name: "John Doe",
+    age: 54,
+    hairColor: "brown",
+    hobbies: ["swimming", "bicycling", "video games"],
+  },
+  {
+    name: "Brenda Smith",
+    age: 33,
+    hairColor: "black",
+    hobbies: ["golf", "mathematics"],
+  },
+  {
+    name: "Jane Garcia",
+    age: 27,
+    hairColor: "blonde",
+    hobbies: ["biology", "medicine", "gymnastics"],
+  },
+];
 
+const products = [
+  {
+    name: "Flat-Screen TV",
+    price: "$300",
+    description: "Huge LCD screen, a great deal",
+    rating: 4.5,
+  },
+  {
+    name: "Basketball",
+    price: "$10",
+    description: "Just like the pros use",
+    rating: 3.8,
+  },
+  {
+    name: "Running Shoes",
+    price: "$120",
+    description: "State-of-the-art technology for optimum running",
+    rating: 4.2,
+  },
+];
+//making reusable list
 function App() {
-	return (
-		<h1>Your code goes here</h1>
-	);
+  return (
+    <>
+      <RegularList
+        items={people}
+        resourceName="person"
+        itemComponent={SmallPersonListItem}
+      />
+      <RegularList
+        items={people}
+        resourceName="person"
+        itemComponent={LargePersonListItem}
+      />
+    </>
+  );
 }
 
 export default App;
--- src/RegularList.js
+++ src/RegularList.js
@@ -1,13 +1,13 @@
 export const RegularList = ({
-	items,
-	resourceName,
-	itemComponent: ItemComponent,
+  items,
+  resourceName,
+  itemComponent: ItemComponent,
 }) => {
-	return (
-		<>
-		{items.map((item, i) => (
-			<ItemComponent key={i} {...{ [resourceName]: item }} />
-		))}
-		</>
-	)
-}
+  return (
+    <>
+      {items.map((item, i) => (
+        <ItemComponent key={i} {...{ [resourceName]: item }} /> //turns into ex. people={items}, which passes through smallpersonlistitem
+      ))}
+    </>
+  );
+};

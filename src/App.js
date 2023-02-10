da6f3e4f20b6fcd83e45fb65f076dd69e39b8b58
jpg22 <jpg22@tamu.edu>
2023-02-10 16:25:26 -0600

    Y

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
+import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
 
 const StepOne = ({ goToNext }) => (
-	<>
-	<h1>Step 1</h1>
-	<button onClick={goToNext}>Next</button>
-	</>
+  <>
+    <h1>Step 1</h1>
+    <button onClick={() => goToNext({ name: "John Doe" })}>Next</button>
+  </>
 );
 const StepTwo = ({ goToNext }) => (
-	<>
-	<h1>Step 2</h1>
-	<button onClick={goToNext}>Next</button>
-	</>
+  <>
+    <h1>Step 2</h1>
+    <button onClick={() => goToNext({ age: 100 })}>Next</button>
+  </>
 );
 const StepThree = ({ goToNext }) => (
-	<>
-	<h1>Step 3</h1>
-	<button onClick={goToNext}>Next</button>
-	</>
+  <>
+    <h1>Step 3</h1>
+    <button onClick={() => goToNext({ hairColor: "brown" })}>Next</button>
+  </>
 );
 
 function App() {
-	return (
-		<UncontrolledOnboardingFlow>
-			<StepOne />
-			<StepTwo />
-			<StepThree />
-		</UncontrolledOnboardingFlow>
-	);
+  return (
+    <UncontrolledOnboardingFlow onFinish={(data) => console.log(data)}>
+      <StepOne />
+      <StepTwo />
+      <StepThree />
+    </UncontrolledOnboardingFlow>
+  );
 }
 
 export default App;

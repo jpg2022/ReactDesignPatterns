3ecf4d56343d612242dba79b6a50cebcde519aae
jpg22 <jpg22@tamu.edu>
2023-02-09 22:57:45 -0600

    Y

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import { UncontrolledForm } from './UncontrolledForm';
-import { ControlledForm } from './ControlledForm';
+import { UncontrolledForm } from "./UncontrolledForm";
+import { ControlledForm } from "./ControlledForm";
+import { UncontrolledModal } from "./UncontrolledModal";
+import { useState } from "react";
+import { ControlledModal } from "./ControlledModal";
 
 function App() {
-	return (
-		<ControlledForm />
-	);
+  const [shouldShowModal, setshouldShowModal] = useState(false);
+  return (
+    <>
+      <ControlledModal
+        shouldShow={shouldShowModal}
+        onRequestClose={() => {
+          setshouldShowModal(false);
+        }}
+      >
+        <h1>Hello!</h1>
+      </ControlledModal>
+      <button onClick={() => setshouldShowModal(!shouldShowModal)}>
+        {shouldShowModal ? "Hide Modal" : "Show Modal"}
+      </button>
+    </>
+  );
 }
 
 export default App;

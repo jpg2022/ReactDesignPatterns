fdd1926bd800c29cb72dbcbe787c10821930de3f
jpg22 <jpg22@tamu.edu>
2023-02-12 10:09:08 -0600

    Y

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { printProps } from "./printProps";
+import { UserInfo } from "./UserInfo";
+
+const UserInfoWrapped = printProps(UserInfo);
+
 function App() {
-	return (
-		<h1>Your code goes here</h1>
-	);
+  return <UserInfoWrapped a={1} b="Hello" c={{ name: "shaun" }} />;
 }
 
 export default App;
